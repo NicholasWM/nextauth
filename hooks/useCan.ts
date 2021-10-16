2dc2930acf920f4f3b3d3c6e77a6090630559afc
NicholasWM <nicholaswesleymazzei@gmail.com>
2021-10-16 09:56:54 -0300

    add: Validando permissões e redirecionamento pelo Server

--- hooks/useCan.ts
+++ hooks/useCan.ts
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { validateUserPermissions } from "../utils/validateUserPermissions";
 
 type UseCamParams = {
     permissions?: string[],
@@ -10,28 +11,10 @@ export function useCan({permissions, roles}: UseCamParams){
     const {isAuthenticated, user} = useContext(AuthContext)
     console.log(permissions, roles);
     console.log(user?.permissions);
-    
     if(!isAuthenticated){
         return false
     }
-
-    if(permissions?.length > 0){
-        const hasAllPermissions = permissions.some(permission => {
-            return user.permissions.includes(permission)
-        });
-        
-        if(!hasAllPermissions){
-            return false
-        }
-    }
-
-    if(roles?.length > 0){
-        const hasAllRoles = roles.some(role => {
-            return user.roles.includes(role)
-        });
-        if(!hasAllRoles){
-            return false
-        }
-    }
-    return true
+    const userHasValidPermissions = validateUserPermissions({user, permissions, roles})
+  
+    return userHasValidPermissions
 }
